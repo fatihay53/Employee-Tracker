64571dd49d88f63dbb85ad9e49d9065a59ef24f8
Fatih Ay <Fatih Ay>
2021-02-21 23:41:00 -0500

    mysql main questions created

--- index.js
+++ index.js
@@ -1,22 +1,7 @@
 const inquirer = require("inquirer");
 
 const db = require('./app/connection')('Cms_Employee_Tracker', 'password123')
-/*
-1. id    
-  2. title
-  3. artist
-  4. genre
-  5. rating
-CREATE TABLE songs (
-  id INTEGER AUTO_INCREMENT PRIMARY KEY,
-  title VARCHAR(200),
-  artist VARCHAR(200),
-  genre VARCHAR(200),
-  rating INTEGER
-)
-*/
 
-// user first question view or manage company
 
 async function viewOrManage() {
 
@@ -38,6 +23,7 @@ if (response.firstChoise === "View Details") {
     function viewDetails()
 }
 else {
+    secondChoise()
 
 }
 
@@ -77,6 +63,14 @@ else if (response1.viewOption === "View the total utilized budget of a departmen
 
 }
 
+async function secondChoise () {
+    await inquirer.prompt([
+        { name: "addUpdateDelete", message: "What would you like to do?", type: 'list' },
+       
+    ]);
+}
+
+
 
 
 
@@ -93,40 +87,4 @@ const newSong = await inquirer.prompt([
 
 
 
-
-
-
-
-async function main() {
-    // get new song info
-    const newSong = await inquirer.prompt([
-        { name: "title", message: "What is the title of the song?" },
-        { name: "artist", message: "What is the artist?" },
-        { name: "genre", message: "What is the genre?" },
-        { name: "rating", message: "What is the rating (1-10)?" },
-    ]);
-    // insert this prompted song
-    let result
-    // create an entry
-    result = await db.query("INSERT INTO songs VALUES(?,?,?,?,?)",
-        [0, newSong.title, newSong.artist, newSong.genre, newSong.rating])
-
-    result = await db.query('SELECT * FROM songs ')
-
-    // console.log(`-- SELECT * result: --\n`, result, "\n\n")
-
-    result = await db.query("INSERT INTO songs VALUES(0,'Enter Sandman','Metallica','Rock',7)")
-
-    result = await db.query("INSERT INTO songs VALUES(0,'Enter Sandman','Metallica','Rock',7)")
-    // ... ?
-    let data
-    var data = await db.query('SELECT * FROM songs');
-    var data = await db.query("SELECT * FROM songs where artist = 'Michael Jackson'");
-
-    // console.log(data)
-
-    console.log(`SELECT * songs`, data)
-    console.log(`-- SELECT * data: --\n`, data, "\n\n")
-    await db.close()
-}
 main();
