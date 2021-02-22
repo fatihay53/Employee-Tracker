672638c0e4e3d10388d92d787ce4a6ccab417a2c
Fatih Ay <Fatih Ay>
2021-02-22 09:55:32 -0500

    question teampleate prepared

--- index.js
+++ index.js
@@ -1,90 +1,132 @@
-const inquirer = require("inquirer");
 
-const db = require('./app/connection')('Cms_Employee_Tracker', 'password123')
+const inquirer = require('inquirer');
+// module to connect to database
+const db = require('./app/connection')('content_management_systems', 'skotkalb')
 
-
-async function viewOrManage() {
-
-    const x = await inquirer.prompt([
-        {
-            type: 'list',
-            message: 'What would you like to do?',
-            choices: [
-                "View Details",
-                "Update/Add/Delete",
-            ],
-            name: 'firstChoise'
-        }
+function InitialQuestion() {
+    // get new song info
+    return inquirer.prompt([
+        { name: "firstChoice", message: "What would you like to do", type: "list", choices: ["View Details", "Update/Add/Delete",] },
     ])
 }
 
-const response = await viewOrManage()
-if (response.firstChoise === "View Details") {
-    function viewDetails()
-}
-else {
-    secondChoise()
-
-}
-
-async function viewDetails() {
-    let listView = await inquirer.prompt([
-        {
-            type: 'list',
-            message: 'What would you like to view?',
-            choices: [
-                "All employees",
-                "Departments",
-                "Roles",
-                "View the total utilized budget of a department",
-            ],
-            name: 'viewOption'
-        }
+function addChoice() {
+    return inquirer.prompt([
+        { name: "addChoice1", message: "What would you like to add", type: "list", choices: ["Employee", "Role", "Department"] },
     ])
-
-const response1 = await viewDetails()
-if (response1.viewOption === "All employees") {
-    let data = await db.query('SELECT * FROM employee');
-    console.log(data)
-    // viewEmployee()
-}
-else if (response1.viewOption === "Departments") {
-    let data = await db.query('SELECT * FROM department');
-    console.log(data)
-}
-else if (response1.viewOption === "Roles") {
-    let data = await db.query('SELECT * FROM role');
-    console.log(data)
-}
-else if (response1.viewOption === "View the total utilized budget of a department"){
-    let data = await db.query('SELECT SUM(salary) FROM role');
-    console.log(data)
-};
-
 }
 
-async function secondChoise () {
-    await inquirer.prompt([
-        { name: "addUpdateDelete", message: "What would you like to do?", type: 'list' },
-       
-    ]);
+function updateChoice() {
+    return inquirer.prompt([
+        {
+            name: "updateChoice1", message: "What would you like to update", type: "list", choices: ["Employee Role", "Employee Manager"
+                , "Employee Department"]
+        },
+    ])
+}
+function deleteChoice() {
+    return inquirer.prompt([
+        {
+            name: "removeChoice1", message: "What would you like to remove", type: "list", choices: ["Employee", "Role"
+                , "Department"]
+        },
+    ])
 }
 
 
+// else{
+
+// }
+
+function viewDetails() {
+
+    return inquirer.prompt([
+        {
+            name: "viewDetails", message: "What would you like to view", type: "list",
+            choices: ["Employees", "Departments", "Roles", "total utilized budget of a department"]
+        },
+    ])
+    // const myResult = await db.query( "SELECT * FROM Employees",);
+    // console.log( `insert result:`, myResult )
+}
 
 
+// const myResult = await db.query( 
+//     "INSERT INTO favourite_songs (title,artist,genre,rating) VALUES(?,?,?,?) ",
+//     [newSong.title, newSong.artist, newSong.genre, newSong.rating] );
+// console.log( `insert result:`, myResult )
+
+// const mySongs = await db.query( "SELECT * FROM favourite_songs" );
+// for( let i=0; i<mySongs.length; i++ ){
+//     const song = mySongs[i];
+//     console.log( `${i}: ${song.title}/${song.artist}  [*${song.genre}*] ${song.rating}/10 `)
+// }
+// console.log( mySongs );
+// -------------
+function secondOption() {
+    return inquirer.prompt([
+        {
+            name: "addUpdateDelete", message: "What would you like to do", type: "list",
+            choices: ["Add", "Update", "Remove"]
+        },
+    ])
+}
+
+async function main() {
+
+    const response1 = await InitialQuestion();
+    console.log(response1)
+    if (response1.firstChoice === "View Details") {
+        const response11 = await viewDetails();
+        if (response11.viewDetails === "Employees") {
+            //  viewEmployees()
+            const myResult = await db.query("SELECT * FROM employee")
+            console.log(myResult)
+        }
+        else if (response11.viewDetails === "Departments") {
+            const myResult = await db.query("SELECT * FROM department")
+            console.log(myResult)
+        }
+        else if (response11.viewDetails === "Roles") {
+            const myResult = await db.query("SELECT * FROM role")
+            console.log(myResult)
+        }
+        else if (response11.viewDetails === "total utilized budget of a department") {
+            const myResult = await db.query("SELECT SUM(salary) FROM role;")
+            console.log(myResult)
+        }
+    }
+    else {
+        const response2 = await secondOption()
+        if (response2.viewDetails === "Add") {
+            const response3 = await addChoice();
+            if (response3.addChoice1 === "Employee") {
+                const updateEmployees = await inquirer.prompt([
+                    { name: "firstName", message: "What is the employee's first name", type: "input" },
+                    { name: "lasttName", message: "What is the employee's last name", type: "input" },
+                    {
+                        name: "role", message: "What is the employee's role", type: "list", choices: ["Sales Lead", "Salesperson", "Lead Engineer",
+                            "Software Engineer", "Account Manager", "Accountant", "Legal Team Lead",]
+                    },
+                    { name: "managerName", message: "In Which department?", type: "list", choices: ["Sales", "Engineering", "Finance", "Legal",] },
+                ])
+                const myResult = await db.query(
+                    "INSERT INTO employee (firstName,lasttName,role,managerName) VALUES(?,?,?,?,?) ",
+                    [0, updateEmployees.firstName, updateEmployees.lasttName, updateEmployees.role, updateEmployees.managerName]);
+                console.log(`insert result:`, myResult)
+            }
+
+        }
+        else if (response2.viewDetails === "Update") {
+            updateChoice()
+        }
+        else if (response2.viewDetails === "Remove") {
+            deleteChoice()
+        }
+
+    }
 
 
-const newSong = await inquirer.prompt([
-    { name: "firstName", message: "What is your employee Name?", type: 'input' },
-    { name: "lastName", message: "What is your employee Lastname?", type: 'input' },
-    { name: "employeeRole", message: "What is your employee Role?", type: 'list', choices: ["Sales Lead", "Sales Person", "Lead Engineer", "Software Engineer", "Account Manager", "Accountant", "Legal Team Lead"] },
-    { name: "department", message: "Which department do you want to add?", type: 'list', choices: ["Sales", "Engineering", "Finance", "Legal"] },
-    { name: "managerName", message: "What is the artist?" },
-    { name: "genre", message: "What is the genre?" },
-    { name: "rating", message: "What is the rating (1-10)?" },
-]);
-
-
-
+    await db.close();
+}
 main();
