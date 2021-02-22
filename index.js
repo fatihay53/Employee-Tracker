5ed37d4803d9f44a18732adb8ec0cd9cda637fee
Fatih Ay <Fatih Ay>
2021-02-21 19:57:32 -0500

    mysql connection succesfully completed and adding details

--- index.js
+++ index.js
@@ -25,25 +25,72 @@ async function viewOrManage() {
             type: 'list',
             message: 'What would you like to do?',
             choices: [
-                "View All Employees",
-                "Update Employess",
+                "View Details",
+                "Update/Add/Delete",
             ],
             name: 'firstChoise'
         }
     ])
 }
+
 const response = await viewOrManage()
-if (response.firstChoise === "View All Employees") {
-    var data = await db.query('SELECT * FROM employee');
+if (response.firstChoise === "View Details") {
+    function viewDetails()
 }
 else {
-    const newSong = await inquirer.prompt([
-        { name: "depart", message: "Which department do you want to add?", type:'list', choices:["Sales","Engineering","Finance","Legal"] },
-        { name: "artist", message: "What is the artist?" },
-        { name: "genre", message: "What is the genre?" },
-        { name: "rating", message: "What is the rating (1-10)?" },
-    ]);
+
 }
+
+async function viewDetails() {
+    let listView = await inquirer.prompt([
+        {
+            type: 'list',
+            message: 'What would you like to view?',
+            choices: [
+                "All employees",
+                "Departments",
+                "Roles",
+                "View the total utilized budget of a department",
+            ],
+            name: 'viewOption'
+        }
+    ])
+
+const response1 = await viewDetails()
+if (response1.viewOption === "All employees") {
+    let data = await db.query('SELECT * FROM employee');
+    console.log(data)
+    // viewEmployee()
+}
+else if (response1.viewOption === "Departments") {
+    let data = await db.query('SELECT * FROM department');
+    console.log(data)
+}
+else if (response1.viewOption === "Roles") {
+    let data = await db.query('SELECT * FROM role');
+    console.log(data)
+}
+else if (response1.viewOption === "View the total utilized budget of a department"){
+    let data = await db.query('SELECT SUM(salary) FROM role');
+    console.log(data)
+};
+
+}
+
+
+
+
+
+const newSong = await inquirer.prompt([
+    { name: "firstName", message: "What is your employee Name?", type: 'input' },
+    { name: "lastName", message: "What is your employee Lastname?", type: 'input' },
+    { name: "employeeRole", message: "What is your employee Role?", type: 'list', choices: ["Sales Lead", "Sales Person", "Lead Engineer", "Software Engineer", "Account Manager", "Accountant", "Legal Team Lead"] },
+    { name: "department", message: "Which department do you want to add?", type: 'list', choices: ["Sales", "Engineering", "Finance", "Legal"] },
+    { name: "managerName", message: "What is the artist?" },
+    { name: "genre", message: "What is the genre?" },
+    { name: "rating", message: "What is the rating (1-10)?" },
+]);
+
 
 
 
