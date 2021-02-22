ab25fd90273d9ecba441650d1f692723b5b838d6
Fatih Ay <Fatih Ay>
2021-02-22 10:07:22 -0500

    question teampleate bug fixed

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 
 const inquirer = require('inquirer');
 // module to connect to database
-const db = require('./app/connection')('content_management_systems', 'skotkalb')
+const db = require('./app/connection')('Cms_Employee_Tracker', 'password123')
 
 function InitialQuestion() {
-    // get new song info
+    // ask question about view/add/update/delete info
     return inquirer.prompt([
         { name: "firstChoice", message: "What would you like to do", type: "list", choices: ["View Details", "Update/Add/Delete",] },
     ])
